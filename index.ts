288f441663a00a6c54a415ad942d0964ed5027c7
Saksorn21 <boat_saksorn@icloud.com>
2024-08-10 16:11:46 +0000

    Add getidxEnv & debug v.1.0.1

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv' 
 
+
+
 declare type Env = {
   [key: string | number | symbol]: PropEnv
 }
@@ -12,10 +14,12 @@ const getEnv = (pathFile?: string[] ) => {
 }
 export default getEnv
 
-type PropEnv = string | number | symbol
+declare type PropEnv = string | number | symbol
+declare type ReturnType = string | number | symbol | undefined
 declare type Envtype = {
-  getOne(nameEnv: PropEnv): void;
-  getAll(): void;
+  getOne(nameEnv: PropEnv): ReturnType;
+  getAll(): ReturnType;
+  getIdx(nameEnv: PropEnv): ReturnType;
 }
 
 const getOneEnv = (nameEnv: string) => {
@@ -26,13 +30,32 @@ const getOneEnv = (nameEnv: string) => {
   return env
 }
 const getAllEnv = () => {
+  const env = process.env;
+  if (Object.keys(env).length === 0) {
+    return undefined;
+  } else {
+    let result = ''; // Initialize an empty string to store the result
+    for (const key in env) {
+      result += `${key}:${env[key]}\n`; // Add each key-value pair to the result string
+     
+    }
+    return result.trim(); // Remove trailing newline and return the result
+  }
+}
+const getIdxEnv = (nameEnv: string) => {
+  
   const env = process.env
-  return env
+  const keys = Object.keys(env)
+  const searchIdx =  keys.find((key) => key === nameEnv)
+  console.log(searchIdx);
+  !searchIdx && console.error(`Environment variable ${nameEnv} is not defined`)
+  return searchIdx
 }
 
 export const env: Envtype = {
   getOne: getOneEnv,
   getAll: getAllEnv,
+  getIdx: getIdxEnv
 
 }
 
